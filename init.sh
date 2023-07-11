954537c5836f2972df9322781840f17f475aea4e
Anthony Tsang <htsang@student.42heilbronn.de>
2023-07-11 13:32:27 +0200

    fix: using DORKER_ECHO+ON_STARTUP variable

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 source $(dirname ${BASH_SOURCE:-$0})/src/settings.sh
 source $(dirname ${BASH_SOURCE:-$0})/src/docker.sh &&
 source $(dirname ${BASH_SOURCE:-$0})/src/dorker.sh &&
-[[ $DORKER_SHOW_LOAD_ON_STARTUP -eq 1 ]] &&
+[[ $DORKER_ECHO_ON_STARTUP -eq 1 ]] &&
 echo $DORKER_GREEN"Dorker commands successfully loaded"$DORKER_WHITE
